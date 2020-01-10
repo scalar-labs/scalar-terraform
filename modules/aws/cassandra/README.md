c4eb2822685a2bc3fd86e895724c1de118341023
Kun Tei <tei@leosys.space>
2020-01-10 14:14:10 +0900

    Add readme for cassandra module

---
+++ modules/aws/cassandra/README.md
@@ -0,0 +1,30 @@
+# Cassandra AWS Module
+The Cassandra AWS module deploys a Cassandra cluster tuned for a Scalar DL environment.
+
+## Internal URL:
+`cassandra-[1-x].internal.scalar-labs.com`
+e.g., `cassandra-[1-x].internal.scalar-labs.com`
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:-----:|
+| base | The base of cassandra | `string` | `"default"` | no |
+| cassandra | The cluster settings of cassandra | `map` | `{}` | no |
+| cassy | The cluster settings of cassy | `map` | `{}` | no |
+| network | The network settings of cassandra | `map` | n/a | yes |
+| reaper | The cluster settings of reaper | `map` | `{}` | no |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| cassandra_host_ids | A list of host ids for the cassandra cluster. |
+| cassandra_host_ips | A list of host IP addresess for the cassandra cluster. |
+| cassandra_hosts | A list of dns urls to access the cassandra cluster. |
+| cassandra_provision_ids | The ID of the provisioning step. |
+| cassandra_resource_count | The number of cassandra seed resources to create. |
+| cassandra_security_ids | The security group ID of the cassandra cluster. |
+| cassandra_seed_ips | A list of host IP addresess for the cassandra seeds. |
+| cassandra_start_on_initial_boot | A flag to start cassandra or not on the initial boot. |
+| network_interface_ids | A list of primary interface id for the cassandra cluster. |
--- modules/aws/cassandra/output.tf
+++ modules/aws/cassandra/output.tf
@@ -1,35 +1,44 @@
 output "cassandra_provision_ids" {
-  value = module.cassandra_provision.provision_ids
+  value       = module.cassandra_provision.provision_ids
+  description = "The ID of the provisioning step."
 }
 
 output "cassandra_host_ips" {
-  value = module.cassandra_cluster.private_ip
+  value       = module.cassandra_cluster.private_ip
+  description = "A list of host IP addresess for the cassandra cluster."
 }
 
 output "cassandra_seed_ips" {
-  value = local.cassandra.resource_count > 0 ? slice(module.cassandra_cluster.private_ip, 0, min(local.cassandra.resource_count, 3)) : []
+  value       = local.cassandra.resource_count > 0 ? slice(module.cassandra_cluster.private_ip, 0, min(local.cassandra.resource_count, 3)) : []
+  description = "A list of host IP addresess for the cassandra seeds."
 }
 
 output "cassandra_host_ids" {
-  value = module.cassandra_cluster.id
+  value       = module.cassandra_cluster.id
+  description = "A list of host ids for the cassandra cluster."
 }
 
 output "cassandra_security_ids" {
-  value = aws_security_group.cassandra.*.id
+  value       = aws_security_group.cassandra.*.id
+  description = "The security group ID of the cassandra cluster."
 }
 
 output "cassandra_hosts" {
-  value = aws_route53_record.cassandra-dns.*.name
+  value       = aws_route53_record.cassandra-dns.*.name
+  description = "A list of dns urls to access the cassandra cluster."
 }
 
 output "network_interface_ids" {
   value = module.cassandra_cluster.primary_network_interface_id
+  description = "A list of primary interface id for the cassandra cluster."
 }
 
 output "cassandra_resource_count" {
-  value = local.cassandra.resource_count
+  value       = local.cassandra.resource_count
+  description = "The number of cassandra seed resources to create."
 }
 
 output "cassandra_start_on_initial_boot" {
-  value = local.cassandra.start_on_initial_boot
+  value       = local.cassandra.start_on_initial_boot
+  description = "A flag to start cassandra or not on the initial boot."
 }

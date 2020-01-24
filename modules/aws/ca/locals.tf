587d95983bbae5d7bd3a2dce9100f175b90f250b
Kun Tei <tei@leosys.space>
2020-01-24 13:06:28 +0900

    Fix tf fmt

--- modules/aws/ca/locals.tf
+++ modules/aws/ca/locals.tf
@@ -1,15 +1,15 @@
 ### General
 locals {
-  network_cidr      = var.network.cidr
-  network_name      = var.network.name
-  network_dns       = var.network.dns
-  network_id        = var.network.id
-  subnet_id         = var.network.subnet_id
-  image_id          = var.network.image_id
-  key_name          = var.network.key_name
-  bastion_ip        = var.network.bastion_ip
-  private_key_path  = var.network.private_key_path
-  user_name         = var.network.user_name
+  network_cidr     = var.network.cidr
+  network_name     = var.network.name
+  network_dns      = var.network.dns
+  network_id       = var.network.id
+  subnet_id        = var.network.subnet_id
+  image_id         = var.network.image_id
+  key_name         = var.network.key_name
+  bastion_ip       = var.network.bastion_ip
+  private_key_path = var.network.private_key_path
+  user_name        = var.network.user_name
 
   triggers = [var.network.bastion_provision_id]
 }

e1b5441cdcd87197243c9303447f9d3e55e5e1db
Kun Tei <tei@leosys.space>
2020-01-24 16:45:05 +0900

    Fix module source path

--- examples/aws/monitor/main.tf
+++ examples/aws/monitor/main.tf
@@ -1,6 +1,6 @@
 module "monitor" {
-  source = "git@github.com:scalar-labs/scalar-terraform.git//modules/aws/monitor?ref=master"
-  #source = "../../../modules/aws/monitor"
+  #source = "git@github.com:scalar-labs/scalar-terraform.git//modules/aws/monitor?ref=master"
+  source = "../../../modules/aws/monitor"
 
   # Required Variables (Use remote state)
   network   = local.network

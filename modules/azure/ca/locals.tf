7f41236d01791ef4625a757403c03c66a40040e9
Kun Tei <tei@leosys.space>
2020-01-24 12:31:54 +0900

    Fix default disk size to 30

--- modules/azure/ca/locals.tf
+++ modules/azure/ca/locals.tf
@@ -20,7 +20,7 @@ locals {
   ca_default = {
     resource_type             = "Standard_B2s"
     resource_count            = 1
-    resource_root_volume_size = 16
+    resource_root_volume_size = 30
     enable_tdagent            = true
   }
 

d4ed0cd39e8ed781acb18c2ca1ed853f083211e2
Kun Tei <tei@leosys.space>
2020-02-05 16:44:03 +0900

    Fix go style

--- test/src/integration/integration_test.go
+++ test/src/integration/integration_test.go
@@ -1,75 +1,74 @@
 package test
 
 import (
-	"testing"
-	"time"
-	"flag"
-	"strings"
+  "testing"
+  "time"
+  "flag"
+  "strings"
 
-	"github.com/gruntwork-io/terratest/modules/logger"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+  "github.com/gruntwork-io/terratest/modules/logger"
+  "github.com/gruntwork-io/terratest/modules/terraform"
+  test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 var terraformDir = flag.String("directory", "", "Directory path of the terraform module to test")
 
 func TestEndToEnd(t *testing.T) {
-	t.Parallel()
-	logger.Logf(t, "Start End To End Test")
+  t.Parallel()
+  logger.Logf(t, "Start End To End Test")
 
-	defer test_structure.RunTestStage(t, "teardown", func() {
-		scalarModules := []string{"monitor", "scalardl", "cassandra", "network"}
+  defer test_structure.RunTestStage(t, "teardown", func() {
+    scalarModules := []string{"monitor", "scalardl", "cassandra", "network"}
 
-		for _, m := range scalarModules {
-			terraformOptions := &terraform.Options{
-				TerraformDir: *terraformDir + m,
-				Vars:         map[string]interface{}{},
-				NoColor:      true,
-			}
+    for _, m := range scalarModules {
+      terraformOptions := &terraform.Options{
+        TerraformDir: *terraformDir + m,
+        Vars:         map[string]interface{}{},
+        NoColor:      true,
+      }
 
       logger.Logf(t, "Destroying <%s> Infrastructure", m)
-			terraform.DestroyE(t, terraformOptions)
+      terraform.DestroyE(t, terraformOptions)
 
-			// [azure only] All resources in the resource group are deleted by first destroy, So skip next destory.
-			if strings.Contains(*terraformDir, "azuredeploy") {
-				logger.Logf(t, "break")
-				break
-			}
-		}
+      // [azure only] All resources in the resource group are deleted by first destroy, So skip next destory.
+      if strings.Contains(*terraformDir, "azuredeploy") {
+        break
+      }
+    }
 
-		logger.Logf(t, "Finished End To End Test")
-	})
+    logger.Logf(t, "Finished End To End Test")
+  })
 
-	test_structure.RunTestStage(t, "setup", func() {
-		scalarModules := []string{"network", "cassandra", "scalardl", "monitor"}
+  test_structure.RunTestStage(t, "setup", func() {
+    scalarModules := []string{"network", "cassandra", "scalardl", "monitor"}
 
-		for _, m := range scalarModules {
-			terraformOptions := &terraform.Options{
-				TerraformDir: *terraformDir + m,
-				Vars:         map[string]interface{}{},
-				NoColor:      true,
-			}
+    for _, m := range scalarModules {
+      terraformOptions := &terraform.Options{
+        TerraformDir: *terraformDir + m,
+        Vars:         map[string]interface{}{},
+        NoColor:      true,
+      }
 
-			logger.Logf(t, "Creating <%s> Infrastructure", m)
-			terraform.InitAndApply(t, terraformOptions)
-		}
+      logger.Logf(t, "Creating <%s> Infrastructure", m)
+      terraform.InitAndApply(t, terraformOptions)
+    }
 
-		logger.Logf(t, "Finished Creating Infrastructure: Tests will continue in 2 minutes")
-		time.Sleep(120 * time.Second)
-	})
+    logger.Logf(t, "Finished Creating Infrastructure: Tests will continue in 2 minutes")
+    time.Sleep(120 * time.Second)
+  })
 
-	test_structure.RunTestStage(t, "validate", func() {
-		t.Run("TestScalarDL", TestScalarDL)
-	  t.Run("TestPrometheusAlerts", TestPrometheusAlerts)
-	})
+  test_structure.RunTestStage(t, "validate", func() {
+    t.Run("TestScalarDL", TestScalarDL)
+    t.Run("TestPrometheusAlerts", TestPrometheusAlerts)
+  })
 }
 
 func lookupTargetValue(t *testing.T, module string, targetValue string) string {
-	terraformOptions := &terraform.Options{
-		TerraformDir: *terraformDir + module,
-		Vars:         map[string]interface{}{},
-		NoColor:      true,
-	}
+  terraformOptions := &terraform.Options{
+    TerraformDir: *terraformDir + module,
+    Vars:         map[string]interface{}{},
+    NoColor:      true,
+  }
 
-	return terraform.OutputRequired(t, terraformOptions, targetValue)
+  return terraform.OutputRequired(t, terraformOptions, targetValue)
 }
--- test/src/modules/grpc_helper/grpc_helper.go
+++ test/src/modules/grpc_helper/grpc_helper.go
@@ -1,139 +1,139 @@
 package grpc_helper
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"os/exec"
-	"testing"
+  "bytes"
+  "encoding/base64"
+  "encoding/json"
+  "fmt"
+  "io/ioutil"
+  "net/http"
+  "os/exec"
+  "testing"
 
-	"github.com/gruntwork-io/terratest/modules/logger"
+  "github.com/gruntwork-io/terratest/modules/logger"
 )
 
 type ResponseStatus struct {
-    Code string `json:"status_code"`
-		// Output string `json:"output"`
-		ErrorMessage string `json:"error_message"`
+  Code string `json:"status_code"`
+  // Output string `json:"output"`
+  ErrorMessage string `json:"error_message"`
 }
 
 func GrpcJavaListContracts(t *testing.T, propertiesFile string) (string, string) {
-	action := "list-contracts"
+  action := "list-contracts"
 
-	options := []string{
-		"--properties", propertiesFile,
-	}
+  options := []string{
+    "--properties", propertiesFile,
+  }
 
-	return GrpcJavaTest(t, action, options...)
+  return GrpcJavaTest(t, action, options...)
 }
 
 func GrpcJavaValidateAsset(t *testing.T, propertiesFile string, assetId string) (string, string) {
-	action := "validate-ledger"
+  action := "validate-ledger"
 
-	options := []string{
-		"--properties", propertiesFile,
-		"--asset-id", assetId,
-	}
+  options := []string{
+    "--properties", propertiesFile,
+    "--asset-id", assetId,
+  }
 
-	return GrpcJavaTest(t, action, options...)
+  return GrpcJavaTest(t, action, options...)
 }
 
 func GrpcJavaExectueContract(t *testing.T, propertiesFile string, contractId string, contractArgument string) (string, string) {
-	action := "execute-contract"
+  action := "execute-contract"
 
-	options := []string{
-		"--properties", propertiesFile,
-		"--contract-id", contractId,
-		"--contract-argument", contractArgument,
-	}
+  options := []string{
+    "--properties", propertiesFile,
+    "--contract-id", contractId,
+    "--contract-argument", contractArgument,
+  }
 
-	return GrpcJavaTest(t, action, options...)
+  return GrpcJavaTest(t, action, options...)
 }
 
 func GrpcJavaRegisterContract(t *testing.T, propertiesFile string, contractId string, contractBinaryName string, contractClassFile string) (string, string) {
-	action := "register-contract"
+  action := "register-contract"
 
-	options := []string{
-		"--properties", propertiesFile,
-		"--contract-id", contractId,
-		"--contract-binary-name", contractBinaryName,
-		"--contract-class-file", contractClassFile,
-	}
+  options := []string{
+    "--properties", propertiesFile,
+    "--contract-id", contractId,
+    "--contract-binary-name", contractBinaryName,
+    "--contract-class-file", contractClassFile,
+  }
 
-	return GrpcJavaTest(t, action, options...)
+  return GrpcJavaTest(t, action, options...)
 }
 
 func GrpcJavaRegisterCert(t *testing.T, propertiesFile string) (string, string) {
-	action := "register-cert"
+  action := "register-cert"
 
-	options := []string{
-		"--properties", propertiesFile,
-	}
+  options := []string{
+    "--properties", propertiesFile,
+  }
 
-	return GrpcJavaTest(t, action, options...)
+  return GrpcJavaTest(t, action, options...)
 }
 
 func GrpcJavaTest(t *testing.T, action string, options ...string) (string, string) {
-	logger.Logf(t, "Starting Java %s %v", action, options)
-	command := fmt.Sprintf(`scalardl-client-sdk/client/bin/%s`, action)
-	cmd := exec.Command(command, options...)
+  logger.Logf(t, "Starting Java %s %v", action, options)
+  command := fmt.Sprintf(`scalardl-client-sdk/client/bin/%s`, action)
+  cmd := exec.Command(command, options...)
 
-	byteOutput, err := cmd.CombinedOutput()
-	if err != nil {
-		// t.Fatal(err)
-	}
+  byteOutput, err := cmd.CombinedOutput()
+  if err != nil {
+  // t.Fatal(err)
+  }
 
-	var response_status = new(ResponseStatus)
-	if err := json.Unmarshal(byteOutput, &response_status); err != nil {
-		t.Fatal(err)
-	}
+  var response_status = new(ResponseStatus)
+  if err := json.Unmarshal(byteOutput, &response_status); err != nil {
+    t.Fatal(err)
+  }
 
-	logger.Logf(t, "%s", response_status.Code)
-	logger.Logf(t, "%s", response_status.ErrorMessage)
+  logger.Logf(t, "%s", response_status.Code)
+  logger.Logf(t, "%s", response_status.ErrorMessage)
 
-	return response_status.Code, response_status.ErrorMessage
+  return response_status.Code, response_status.ErrorMessage
 }
 
 func GrpcWebTest(t *testing.T, url string, data string) (int, string) {
-	statusCode, body, err := GrpcWebTestE(t, url, data)
-	if err != nil {
-		t.Fatal(err)
-	}
+  statusCode, body, err := GrpcWebTestE(t, url, data)
+  if err != nil {
+    t.Fatal(err)
+  }
 
-	return statusCode, body
+  return statusCode, body
 }
 
 func GrpcWebTestE(t *testing.T, url string, data string) (int, string, error) {
-	logger.Logf(t, "Making GRPC_WEB call to URL %s", url)
+  logger.Logf(t, "Making GRPC_WEB call to URL %s", url)
 
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, bytes.NewBufferString(data))
+  client := &http.Client{}
+  req, err := http.NewRequest("POST", url, bytes.NewBufferString(data))
 
-	req.Header.Add("Content-Type", "application/grpc-web-text")
-	req.Header.Add("X-User-Agent", "grpc-web-javascript/0.1")
-	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("Accept", "application/grpc-web-text")
-	req.Header.Add("X-Grpc-Web", "1")
+  req.Header.Add("Content-Type", "application/grpc-web-text")
+  req.Header.Add("X-User-Agent", "grpc-web-javascript/0.1")
+  req.Header.Add("Connection", "keep-alive")
+  req.Header.Add("Accept", "application/grpc-web-text")
+  req.Header.Add("X-Grpc-Web", "1")
 
-	resp, err := client.Do(req)
-	if err != nil {
-		return -1, "", err
-	}
+  resp, err := client.Do(req)
+  if err != nil {
+    return -1, "", err
+  }
 
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return -1, "", err
-	}
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return -1, "", err
+  }
 
-	logger.Logf(t, "%s", string(body))
-	message, _ := base64.StdEncoding.DecodeString(string(body))
+  logger.Logf(t, "%s", string(body))
+  message, _ := base64.StdEncoding.DecodeString(string(body))
 
-	logger.Logf(t, "Reponse Status: %s", resp.Status)
-	logger.Logf(t, "Reponse Body: %s", string(body))
-	logger.Logf(t, "Reponse Message: %s", string(message))
+  logger.Logf(t, "Reponse Status: %s", resp.Status)
+  logger.Logf(t, "Reponse Body: %s", string(body))
+  logger.Logf(t, "Reponse Message: %s", string(message))
 
-	return resp.StatusCode, string(message), nil
+  return resp.StatusCode, string(message), nil
 }

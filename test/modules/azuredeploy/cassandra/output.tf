8d5debf3732cfaad1bf3dcb5210f6bccea74bb1b
Kun Tei <tei@leosys.space>
2020-02-05 12:45:03 +0900

    Add cassandra output for test

--- test/modules/azuredeploy/cassandra/output.tf
+++ test/modules/azuredeploy/cassandra/output.tf
@@ -9,3 +9,11 @@ output "cassandra_provision_ids" {
 output "cassandra_start_on_initial_boot" {
   value = module.cassandra.cassandra_start_on_initial_boot
 }
+
+output "cassandra_test_ip_0" {
+  value = module.cassandra.cassandra_host_ips[0]
+}
+
+output "cassandra_test_ip_1" {
+  value = module.cassandra.cassandra_host_ips[1]
+}

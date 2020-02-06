3d91d8bd7d0c9c56901e28698f271cb5ea5f650f
Kun Tei <tei@leosys.space>
2020-02-06 12:49:30 +0900

    Fix go fmt

--- test/src/integration/integration_scalardl_test.go
+++ test/src/integration/integration_scalardl_test.go
@@ -1,73 +1,73 @@
 package test
 
 import (
-  "fmt"
-  "io/ioutil"
-  "testing"
+	"fmt"
+	"io/ioutil"
+	"testing"
 
-  "github.com/gruntwork-io/terratest/modules/logger"
-  "github.com/stretchr/testify/assert"
-  "modules/grpc_helper"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/stretchr/testify/assert"
+	"modules/grpc_helper"
 )
 
 func TestScalarDL(t *testing.T) {
-  t.Run("scalardl", func(t *testing.T) {
-    t.Run("ScalarDLWithJavaClient", TestScalarDLWithJavaClientExpectStatusCodeIsValid)
-    t.Run("ScalarDLWithGrpcWebClient", TestScalarDLWithGrpcWebClientExpectStatusCodeIsValid)
-  })
+	t.Run("scalardl", func(t *testing.T) {
+		t.Run("ScalarDLWithJavaClient", TestScalarDLWithJavaClientExpectStatusCodeIsValid)
+		t.Run("ScalarDLWithGrpcWebClient", TestScalarDLWithGrpcWebClientExpectStatusCodeIsValid)
+	})
 }
 
 func TestScalarDLWithJavaClientExpectStatusCodeIsValid(t *testing.T) {
-  expectedRegisterCertStatusCode := []string{"OK", "CERTIFICATE_ALREADY_REGISTERED"}
-  expectedRegisterContractStatusCode := []string{"OK", "CONTRACT_ALREADY_REGISTERED"}
-  expectedExecuteContractStatusCode := "OK"
-  expectedValidateLedgerStatusCode := "OK"
-  expectedListContractsStatusCode := "OK"
+	expectedRegisterCertStatusCode := []string{"OK", "CERTIFICATE_ALREADY_REGISTERED"}
+	expectedRegisterContractStatusCode := []string{"OK", "CONTRACT_ALREADY_REGISTERED"}
+	expectedExecuteContractStatusCode := "OK"
+	expectedValidateLedgerStatusCode := "OK"
+	expectedListContractsStatusCode := "OK"
 
-  contractID := "test-contract1"
-  contractBinaryName := "com.org1.contract.StateUpdater"
-  contractClassFile := "./resources/StateUpdater.class"
-  assetID := "over9000"
-  assetArgument := fmt.Sprintf(`{"asset_id": "%s", "state": 9001}`, assetID)
-  propertiesFile := "./resources/test.properties"
+	contractID := "test-contract1"
+	contractBinaryName := "com.org1.contract.StateUpdater"
+	contractClassFile := "./resources/StateUpdater.class"
+	assetID := "over9000"
+	assetArgument := fmt.Sprintf(`{"asset_id": "%s", "state": 9001}`, assetID)
+	propertiesFile := "./resources/test.properties"
 
-  scalarurl := lookupTargetValue(t, "scalardl", "envoy_dns")
-  logger.Logf(t, "URL: %s", scalarurl)
-  writePropertiesFile(t, scalarurl)
+	scalarurl := lookupTargetValue(t, "scalardl", "envoy_dns")
+	logger.Logf(t, "URL: %s", scalarurl)
+	writePropertiesFile(t, scalarurl)
 
-  code, _ := grpc_helper.GrpcJavaRegisterCert(t, propertiesFile)
-  assert.Contains(t, expectedRegisterCertStatusCode, code)
+	code, _ := grpc_helper.GrpcJavaRegisterCert(t, propertiesFile)
+	assert.Contains(t, expectedRegisterCertStatusCode, code)
 
-  code, _ = grpc_helper.GrpcJavaRegisterContract(t, propertiesFile, contractID, contractBinaryName, contractClassFile)
-  assert.Contains(t, expectedRegisterContractStatusCode, code)
+	code, _ = grpc_helper.GrpcJavaRegisterContract(t, propertiesFile, contractID, contractBinaryName, contractClassFile)
+	assert.Contains(t, expectedRegisterContractStatusCode, code)
 
-  code, _ = grpc_helper.GrpcJavaExectueContract(t, propertiesFile, contractID, assetArgument)
-  assert.Equal(t, expectedExecuteContractStatusCode, code)
+	code, _ = grpc_helper.GrpcJavaExectueContract(t, propertiesFile, contractID, assetArgument)
+	assert.Equal(t, expectedExecuteContractStatusCode, code)
 
-  code, _ = grpc_helper.GrpcJavaValidateAsset(t, propertiesFile, assetID)
-  assert.Equal(t, expectedValidateLedgerStatusCode, code)
+	code, _ = grpc_helper.GrpcJavaValidateAsset(t, propertiesFile, assetID)
+	assert.Equal(t, expectedValidateLedgerStatusCode, code)
 
-  code, _ = grpc_helper.GrpcJavaListContracts(t, propertiesFile)
-  assert.Equal(t, expectedListContractsStatusCode, code)
+	code, _ = grpc_helper.GrpcJavaListContracts(t, propertiesFile)
+	assert.Equal(t, expectedListContractsStatusCode, code)
 }
 
 func TestScalarDLWithGrpcWebClientExpectStatusCodeIsValid(t *testing.T) {
-  expectedStatusCode := 200
+	expectedStatusCode := 200
 
-  scalarurl := lookupTargetValue(t, "scalardl", "envoy_dns")
-  logger.Logf(t, "URL: %s", scalarurl)
+	scalarurl := lookupTargetValue(t, "scalardl", "envoy_dns")
+	logger.Logf(t, "URL: %s", scalarurl)
 
-  //Register Certificate
-  requestData := "AAAAAFIKBW1kYm94EAEiRzBFAiEAx4josbxWPEuZ7w/imnl5B/xY0FKbQLkuK/E/UFUHbmwCIGBludc3JD3pkTYqmeT186g+rDaoFkLqHg4xCQ8uCL3w"
-  listContractURL := fmt.Sprintf(`http://%s:50051/rpc.Ledger/ListContracts`, scalarurl)
+	//Register Certificate
+	requestData := "AAAAAFIKBW1kYm94EAEiRzBFAiEAx4josbxWPEuZ7w/imnl5B/xY0FKbQLkuK/E/UFUHbmwCIGBludc3JD3pkTYqmeT186g+rDaoFkLqHg4xCQ8uCL3w"
+	listContractURL := fmt.Sprintf(`http://%s:50051/rpc.Ledger/ListContracts`, scalarurl)
 
-  statuCode, _ := grpc_helper.GrpcWebTest(t, listContractURL, requestData)
+	statuCode, _ := grpc_helper.GrpcWebTest(t, listContractURL, requestData)
 
-  assert.Equal(t, expectedStatusCode, statuCode)
+	assert.Equal(t, expectedStatusCode, statuCode)
 }
 
 func writePropertiesFile(t *testing.T, host string) {
-  properties := []byte(fmt.Sprintf(`
+	properties := []byte(fmt.Sprintf(`
   scalar.dl.client.server.host=%s
   scalar.dl.client.server.port=50051
   scalar.dl.client.server.privileged_port=50051
@@ -77,8 +77,8 @@ func writePropertiesFile(t *testing.T, host string) {
   scalar.dl.client.private_key_path=./resources/Test-key.pem
   `, host))
 
-  err := ioutil.WriteFile("./resources/test.properties", properties, 0644)
-  if err != nil {
-    t.Fatal(err)
-  }
+	err := ioutil.WriteFile("./resources/test.properties", properties, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
--- test/src/integration/integration_test.go
+++ test/src/integration/integration_test.go
@@ -1,69 +1,69 @@
 package test
 
 import (
-  "testing"
-  "time"
-  "flag"
-  "strings"
+	"flag"
+	"strings"
+	"testing"
+	"time"
 
-  "github.com/gruntwork-io/terratest/modules/logger"
-  "github.com/gruntwork-io/terratest/modules/terraform"
-  test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
+	"github.com/gruntwork-io/terratest/modules/logger"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
 
 var terraformDir = flag.String("directory", "", "Directory path of the terraform module to test")
 
 func TestEndToEnd(t *testing.T) {
-  t.Parallel()
-  logger.Logf(t, "Start End To End Test")
+	t.Parallel()
+	logger.Logf(t, "Start End To End Test")
 
-  defer test_structure.RunTestStage(t, "teardown", func() {
-    scalarModules := []string{"monitor", "scalardl", "cassandra", "network"}
+	defer test_structure.RunTestStage(t, "teardown", func() {
+		scalarModules := []string{"monitor", "scalardl", "cassandra", "network"}
 
-    for _, m := range scalarModules {
-      terraformOptions := &terraform.Options{
-        TerraformDir: *terraformDir + m,
-        Vars:         map[string]interface{}{},
-        NoColor:      true,
-      }
+		for _, m := range scalarModules {
+			terraformOptions := &terraform.Options{
+				TerraformDir: *terraformDir + m,
+				Vars:         map[string]interface{}{},
+				NoColor:      true,
+			}
 
-      logger.Logf(t, "Destroying <%s> Infrastructure", m)
-      terraform.DestroyE(t, terraformOptions)
-    }
+			logger.Logf(t, "Destroying <%s> Infrastructure", m)
+			terraform.DestroyE(t, terraformOptions)
+		}
 
-    logger.Logf(t, "Finished End To End Test")
-  })
+		logger.Logf(t, "Finished End To End Test")
+	})
 
-  test_structure.RunTestStage(t, "setup", func() {
-    scalarModules := []string{"network", "cassandra", "scalardl", "monitor"}
+	test_structure.RunTestStage(t, "setup", func() {
+		scalarModules := []string{"network", "cassandra", "scalardl", "monitor"}
 
-    for _, m := range scalarModules {
-      terraformOptions := &terraform.Options{
-        TerraformDir: *terraformDir + m,
-        Vars:         map[string]interface{}{},
-        NoColor:      true,
-      }
+		for _, m := range scalarModules {
+			terraformOptions := &terraform.Options{
+				TerraformDir: *terraformDir + m,
+				Vars:         map[string]interface{}{},
+				NoColor:      true,
+			}
 
-      logger.Logf(t, "Creating <%s> Infrastructure", m)
-      terraform.InitAndApply(t, terraformOptions)
-    }
+			logger.Logf(t, "Creating <%s> Infrastructure", m)
+			terraform.InitAndApply(t, terraformOptions)
+		}
 
-    logger.Logf(t, "Finished Creating Infrastructure: Tests will continue in 2 minutes")
-    time.Sleep(120 * time.Second)
-  })
+		logger.Logf(t, "Finished Creating Infrastructure: Tests will continue in 2 minutes")
+		time.Sleep(120 * time.Second)
+	})
 
-  test_structure.RunTestStage(t, "validate", func() {
-    t.Run("TestScalarDL", TestScalarDL)
-    t.Run("TestPrometheusAlerts", TestPrometheusAlerts)
-  })
+	test_structure.RunTestStage(t, "validate", func() {
+		t.Run("TestScalarDL", TestScalarDL)
+		t.Run("TestPrometheusAlerts", TestPrometheusAlerts)
+	})
 }
 
 func lookupTargetValue(t *testing.T, module string, targetValue string) string {
-  terraformOptions := &terraform.Options{
-    TerraformDir: *terraformDir + module,
-    Vars:         map[string]interface{}{},
-    NoColor:      true,
-  }
+	terraformOptions := &terraform.Options{
+		TerraformDir: *terraformDir + module,
+		Vars:         map[string]interface{}{},
+		NoColor:      true,
+	}
 
-  return terraform.OutputRequired(t, terraformOptions, targetValue)
+	return terraform.OutputRequired(t, terraformOptions, targetValue)
 }

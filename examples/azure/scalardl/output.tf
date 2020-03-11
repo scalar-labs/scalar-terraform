43aefbb193b6bec9a56800e8808196cebb5791f9
Kun Tei <tei@leosys.space>
2020-03-11 19:33:12 +0900

    Fix output fail

--- examples/azure/scalardl/output.tf
+++ examples/azure/scalardl/output.tf
@@ -10,18 +10,18 @@ output "scalardl_replication_factor" {
   value = module.scalardl.scalardl_replication_factor
 }
 
-output "envoy_dns" {
-  value = module.scalardl.*.envoy_dns
-}
-
-output "envoy_listen_port" {
-  value = local.envoy.listen_port
-}
+# output "envoy_dns" {
+#   value = module.scalardl.envoy_dns[0]
+# }
+#
+# output "envoy_listen_port" {
+#   value = module.scalardl.envoy_listen_port[0]
+# }
 
 output "scalardl_dns" {
-  value = module.scalardl.*.scalardl_lb_dns
+  value = module.scalardl.scalardl_lb_dns[0]
 }
 
 output "scalardl_listen_port" {
-  value = local.scalardl.listen_port
+  value = module.scalardl.scalardl_lb_listen_port[0]
 }

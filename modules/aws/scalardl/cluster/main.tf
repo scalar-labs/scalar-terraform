d54f17809577f351a7e655c51316721e50a8b13b
tei-k <tei@leosys.space>
2020-03-17 14:35:28 +0900

    Fix hostname for scalardl cluster

--- modules/aws/scalardl/cluster/main.tf
+++ modules/aws/scalardl/cluster/main.tf
@@ -11,7 +11,7 @@ module "scalardl_cluster" {
   vpc_security_group_ids      = var.security_group_ids
   subnet_id                   = var.subnet_id
   associate_public_ip_address = false
-  hostname_prefix             = "scalar"
+  hostname_prefix             = "scalar-${var.resource_cluster_name}"
 
   tags = {
     Terraform = true

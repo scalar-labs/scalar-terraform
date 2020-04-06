c69d8e164001ac37736e55eaed202b2778a50e0b
tei-k <tei@leosys.space>
2020-04-06 15:51:31 +0900

    Fix monitor module for multi-az

--- modules/aws/monitor/main.tf
+++ modules/aws/monitor/main.tf
@@ -9,7 +9,7 @@ module "monitor_cluster" {
   key_name                    = local.key_name
   monitoring                  = false
   vpc_security_group_ids      = aws_security_group.monitor.*.id
-  subnet_id                   = local.subnet_id
+  subnet_ids                  = local.subnet_ids
   associate_public_ip_address = false
   hostname_prefix             = "monitor"
 

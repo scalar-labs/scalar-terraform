b41e894f14a2cb31fde60864160cf096ea7f3774
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-04-07 15:13:38 +0900

    Fix variable name SCALAR_CASSANDRA_USERNAME

--- modules/universal/scalardl/provision/create_schema.sh
+++ modules/universal/scalardl/provision/create_schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cqlsh -u "${SCALAR_CASSANDRA_USER}" \
+cqlsh -u "${SCALAR_CASSANDRA_USERNAME}" \
       -p "${SCALAR_CASSANDRA_PASSWORD}" \
       -f ./create_schema.cql \
       "${SCALAR_CASSANDRA_HOST}" "${SCALAR_CASSANDRA_PORT}"

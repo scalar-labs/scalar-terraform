49e72e47c1cf5eee6e700e033011764ea55436a5
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-05-07 15:58:14 +0900

    Add vm_ids variable to universal/ca module

--- modules/universal/ca/vars.tf
+++ modules/universal/ca/vars.tf
@@ -15,6 +15,10 @@ variable "triggers" {
   default     = []
 }
 
+variable "vm_ids" {
+  default = []
+}
+
 variable "host_list" {
   default     = []
   description = "A list of CA hosts (IP or DNS) to provision"

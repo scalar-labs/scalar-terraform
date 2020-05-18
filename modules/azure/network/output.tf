d2526210804cdb4027ee4233fdcf9c0a6dd1bbd0
tei-k <tei@leosys.space>
2020-05-18 16:06:05 +0900

    Fix network output

--- modules/azure/network/output.tf
+++ modules/azure/network/output.tf
@@ -45,7 +45,7 @@ output "public_key_path" {
 }
 
 output "additional_public_keys_path" {
-  value       = length(var.additional_public_keys_path) > 0 ? abspath(pathexpand(var.additional_public_keys_path)) : ""
+  value       = trim(var.additional_public_keys_path) == "" ? "" : abspath(pathexpand(trim(var.additional_public_keys_path)))
   description = "The path to a file that contains multiple public keys for SSH access."
 }
 

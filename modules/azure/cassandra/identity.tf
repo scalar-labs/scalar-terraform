b56dd306fcc6f62813e5662c9f7fb28c26b605e7
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-05-22 10:32:32 +0900

    Fix data source name to use _

--- modules/azure/cassandra/identity.tf
+++ modules/azure/cassandra/identity.tf
@@ -4,7 +4,7 @@ data "azurerm_role_definition" "contributor" {
   name = "Storage Blob Data Contributor"
 }
 
-data "azurerm_storage_account" "cassy-storage-account" {
+data "azurerm_storage_account" "cassy_storage_account" {
   # If the resource_count of Cassy > 0, this resource makes local.cassy.storage_account_name required and ensures it is accessible.
   count = local.cassy.resource_count > 0 ? 1 : 0
 
@@ -15,15 +15,15 @@ data "azurerm_storage_account" "cassy-storage-account" {
 resource "azurerm_role_assignment" "cassy" {
   count = local.cassy.resource_count
 
-  scope              = data.azurerm_storage_account.cassy-storage-account[0].id
+  scope              = data.azurerm_storage_account.cassy_storage_account[0].id
   role_definition_id = "${data.azurerm_subscription.subscription.id}${data.azurerm_role_definition.contributor.id}"
   principal_id       = module.cassy_cluster.vm_identities[count.index][0]["principal_id"]
 }
 
 resource "azurerm_role_assignment" "cassandra" {
-  count = length(data.azurerm_storage_account.cassy-storage-account) > 0 ? local.cassandra.resource_count : 0
+  count = length(data.azurerm_storage_account.cassy_storage_account) > 0 ? local.cassandra.resource_count : 0
 
-  scope              = data.azurerm_storage_account.cassy-storage-account[0].id
+  scope              = data.azurerm_storage_account.cassy_storage_account[0].id
   role_definition_id = "${data.azurerm_subscription.subscription.id}${data.azurerm_role_definition.contributor.id}"
   principal_id       = module.cassandra_cluster.vm_identities[count.index][0]["principal_id"]
 }

a4a9fbad0551183b5477c44a4c11d609009d46af
tei-k <tei@leosys.space>
2020-06-23 12:24:04 +0900

    Fix cassandra list

--- modules/universal/monitor/locals.tf
+++ modules/universal/monitor/locals.tf
@@ -1,9 +1,10 @@
 locals {
-  scalardl_targets  = contains(var.targets, "scalardl") ? ["scalardl-blue", "scalardl-green", "envoy"] : []
-  cassandra_targets = contains(var.targets, "cassandra") ? ["cassandra", "cassy", "reaper"] : []
-  monitor_targets   = ["grafana", "alertmanager", "prometheus", "nginx"]
+  scalardl_targets      = contains(var.targets, "scalardl") ? ["scalardl-blue", "scalardl-green", "envoy"] : []
+  cassandra_gen_targets = contains(var.targets, "cassandra") ? ["cassandra"] : []
+  cassandra_ops_targets = contains(var.targets, "cassandra") ? ["cassy", "reaper"] : []
+  monitor_targets       = ["grafana", "alertmanager", "prometheus", "nginx"]
 
-  service_targets   = setunion(local.scalardl_targets, local.cassandra_targets)
-  node_targets      = setunion(local.service_targets, ["bastion", "monitor"])
-  cadvisor_targets  = setunion(local.service_targets, ["monitor"])
+  service_targets  = setunion(local.scalardl_targets, local.cassandra_gen_targets, local.cassandra_ops_targets)
+  node_targets     = setunion(local.service_targets, ["bastion", "monitor"])
+  cadvisor_targets = setunion(local.scalardl_targets, local.cassandra_ops_targets, ["monitor"])
 }

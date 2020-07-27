30c307ba60c682731e8b74934a2d5bc67bf30489
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-07-27 12:27:49 +0900

    Associate AWS IAM role with Cassy instance for uploading metadata backup

--- modules/aws/cassandra/cassy.tf
+++ modules/aws/cassandra/cassy.tf
@@ -12,6 +12,7 @@ module "cassy_cluster" {
   subnet_ids                  = local.subnet_ids
   associate_public_ip_address = false
   hostname_prefix             = "cassy"
+  iam_instance_profile        = aws_iam_instance_profile.cassandra.name
   use_num_suffix              = true
 
   tags = merge(

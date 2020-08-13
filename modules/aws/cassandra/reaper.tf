0113990d97786d41e58a575c3a8e4503d6353339
tei-k <tei@leosys.space>
2020-08-13 18:05:55 +0900

    Fix name miss for replication_factor

--- modules/aws/cassandra/reaper.tf
+++ modules/aws/cassandra/reaper.tf
@@ -49,7 +49,7 @@ module "reaper_provision" {
   user_name          = local.user_name
   private_key_path   = local.private_key_path
   provision_count    = local.reaper.resource_count
-  replication_factor = local.reaper.repliation_factor
+  replication_factor = local.reaper.replication_factor
   enable_tdagent     = local.reaper.enable_tdagent
   internal_domain    = local.internal_domain
   cassandra_username = local.reaper.cassandra_username

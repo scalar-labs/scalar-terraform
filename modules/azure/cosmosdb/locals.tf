550394d1e05e1eb17c59aed3f811235b211eaab6
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2020-09-18 12:31:56 +0900

    Removed unused variables from cosmosdb/locals.tf

--- modules/azure/cosmosdb/locals.tf
+++ modules/azure/cosmosdb/locals.tf
@@ -1,12 +1,6 @@
 ### General
 locals {
-  network_cidr        = var.network.cidr
   network_name        = var.network.name
-  network_dns         = var.network.dns
-  network_id          = var.network.id
   region              = var.network.region
   node_pool_subnet_id = var.kubernetes.node_pool_subnet_id
-  bastion_ip          = var.network.bastion_ip
-  internal_domain     = var.network.internal_domain
-  triggers            = [var.network.bastion_provision_id]
 }

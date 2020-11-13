17d1af4cdce6e80cedbc94fb7d95c323006f564c
tei-k <tei@leosys.space>
2020-11-13 18:04:24 +0900

    Add kube-system fargate

--- modules/aws/kubernetes/fargate.tf
+++ modules/aws/kubernetes/fargate.tf
@@ -1,21 +1,3 @@
-module "scalardl_apps_fargate" {
-  source = "./fargate"
-
-  name             = "scalardl"
-  cluster_name     = aws_eks_cluster.eks_cluster.name
-  subnets          = local.kubernetes_scalar_apps_fargate.subnet_ids
-  namespace        = local.kubernetes_scalar_apps_fargate.namespace
-  kubernetes_labes = local.kubernetes_scalar_apps_fargate.kubernetes_labels
-  create_enable    = local.kubernetes_cluster.use_fargate_profile
-
-  tags = var.custom_tags
-
-  eks_depends_on = [
-    aws_eks_cluster.eks_cluster,
-    kubernetes_config_map.aws_auth,
-  ]
-}
-
 module "default_fargate" {
   source = "./fargate"
 
@@ -31,7 +13,44 @@ module "default_fargate" {
   eks_depends_on = [
     aws_eks_cluster.eks_cluster,
     kubernetes_config_map.aws_auth,
-    module.scalardl_apps_fargate,
+  ]
+}
+
+module "kube_system" {
+  source = "./fargate"
+
+  name             = "kube-system"
+  cluster_name     = aws_eks_cluster.eks_cluster.name
+  subnets          = local.subnet_ids
+  namespace        = "kube-system"
+  kubernetes_labes = { infrastructure = "fargate" }
+  create_enable    = local.kubernetes_cluster.use_fargate_profile
+
+  tags = var.custom_tags
+
+  eks_depends_on = [
+    aws_eks_cluster.eks_cluster,
+    kubernetes_config_map.aws_auth,
+    module.default_fargate
+  ]
+}
+
+module "scalardl_apps_fargate" {
+  source = "./fargate"
+
+  name             = "scalardl"
+  cluster_name     = aws_eks_cluster.eks_cluster.name
+  subnets          = local.kubernetes_scalar_apps_fargate.subnet_ids
+  namespace        = local.kubernetes_scalar_apps_fargate.namespace
+  kubernetes_labes = local.kubernetes_scalar_apps_fargate.kubernetes_labels
+  create_enable    = local.kubernetes_cluster.use_fargate_profile
+
+  tags = var.custom_tags
+
+  eks_depends_on = [
+    aws_eks_cluster.eks_cluster,
+    kubernetes_config_map.aws_auth,
+    module.kube_system
   ]
 }
 
@@ -50,6 +69,6 @@ module "monitoring_fargate" {
   eks_depends_on = [
     aws_eks_cluster.eks_cluster,
     kubernetes_config_map.aws_auth,
-    module.default_fargate,
+    module.scalardl_apps_fargate,
   ]
 }

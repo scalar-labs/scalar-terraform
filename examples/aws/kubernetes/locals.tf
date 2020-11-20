e521405c986fffc0b8db5385cf4a4c0efb8b50a5
tei-k <tei@leosys.space>
2020-11-20 18:38:16 +0900

    Revert "Delete unnecessary variables"
    This reverts commit 3d452bbb2932d0bd3ab3f573dbaa39f5c311c28d.

--- examples/aws/kubernetes/locals.tf
+++ examples/aws/kubernetes/locals.tf
@@ -2,12 +2,18 @@ locals {
   network = {
     cidr   = data.terraform_remote_state.network.outputs.network_cidr
     name   = data.terraform_remote_state.network.outputs.network_name
+    dns    = data.terraform_remote_state.network.outputs.network_dns
     id     = data.terraform_remote_state.network.outputs.network_id
     region = data.terraform_remote_state.network.outputs.region
 
     public_subnet_ids  = join(",", data.terraform_remote_state.network.outputs.subnet_map["public"])
     private_subnet_ids = join(",", data.terraform_remote_state.network.outputs.subnet_map["private"])
     subnet_ids         = join(",", data.terraform_remote_state.network.outputs.subnet_map["kubernetes"])
+    key_name           = data.terraform_remote_state.network.outputs.key_name
+
+    bastion_ip      = data.terraform_remote_state.network.outputs.bastion_ip
+    user_name       = data.terraform_remote_state.network.outputs.user_name
+    internal_domain = data.terraform_remote_state.network.outputs.internal_domain
   }
 
   custom_tags = data.terraform_remote_state.network.outputs.custom_tags

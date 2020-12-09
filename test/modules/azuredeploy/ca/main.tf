d876863e3295051491e240daf4b084a11c8987ff
tei-k <tei@leosys.space>
2020-12-09 17:27:21 +0900

    Remove terratest

--- test/modules/azuredeploy/ca/main.tf
+++
@@ -1,9 +0,0 @@
-module "ca" {
-  source = "../../../../modules/azure/ca"
-
-  # Required Variables (Use remote state)
-  network = local.network
-
-  # Optional Variables
-  ca = var.ca
-}
--- test/modules/azuredeploy/cassandra/terraform.tfvars
+++
@@ -1,8 +0,0 @@
-cassandra = {
-  start_on_initial_boot = true
-}
-
-cassy = {
-  resource_root_volume_size = "16"
-  use_managed_identity      = "false"
-}

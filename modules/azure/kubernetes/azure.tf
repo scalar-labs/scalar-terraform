46de4a0e7bcf239a22b8bdf66ea18094582376cb
tei-k <tei@leosys.space>
2021-02-22 15:16:45 +0900

    Upgrade terraform for azure kubernetes

--- modules/azure/kubernetes/azure.tf
+++
@@ -1,8 +0,0 @@
-provider "azurerm" {
-  version = ">= 2.21.0"
-  features {}
-}
-
-provider "azuread" {
-  version = ">= 0.11.0"
-}
--- modules/azure/kubernetes/versions.tf
+++ modules/azure/kubernetes/versions.tf
@@ -1,3 +1,15 @@
 terraform {
-  required_version = ">= 0.12.20"
+  required_version = "~> 0.14.7"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.48"
+    }
+
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 1.4"
+    }
+  }
 }

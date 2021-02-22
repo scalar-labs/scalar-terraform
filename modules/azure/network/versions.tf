be27e02489c93fc28f3eb9d9cbce17733be5b820
tei-k <tei@leosys.space>
2021-02-22 11:17:00 +0900

    Fix requierd provider versions

--- modules/azure/network/versions.tf
+++ modules/azure/network/versions.tf
@@ -1,9 +1,20 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 0.14.7"
 
   required_providers {
     azurerm = {
+      source  = "hashicorp/azurerm"
       version = "=1.38.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.3"
     }
   }
 }

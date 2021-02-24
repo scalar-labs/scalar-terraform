ea887b5fe42c0fa80e77cd98284ee95eadea2bc5
tei-k <tei@leosys.space>
2021-02-24 14:34:10 +0900

    Upgrade terraform for aws kubernetes

--- modules/aws/kubernetes/auth.tf
+++ modules/aws/kubernetes/auth.tf
@@ -40,7 +40,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.eks_cluster.token
   load_config_file       = false
-  version                = "~> 1.11"
 }
 
 resource "null_resource" "wait_for_cluster" {
---
+++ modules/aws/kubernetes/versions.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "~> 0.14.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.13"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1"
+    }
+  }
+}

846e355172a0f77ffe2e0ea2449aaf34029fa4bb
tei-k <tei@leosys.space>
2021-02-24 12:06:06 +0900

    Upgrade terraform to 0.14 for aws

--- modules/aws/network/versions.tf
+++ modules/aws/network/versions.tf
@@ -1,3 +1,25 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 0.14.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2"
+    }
+  }
 }

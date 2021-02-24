f5c50eb8202d25db32548ae7a0936fdd67f594da
tei-k <tei@leosys.space>
2021-02-24 13:47:21 +0900

    Upgrade terraform for aws scalardl

--- modules/aws/scalardl/versions.tf
+++ modules/aws/scalardl/versions.tf
@@ -1,3 +1,20 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 0.14.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.29"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2"
+    }
+  }
 }

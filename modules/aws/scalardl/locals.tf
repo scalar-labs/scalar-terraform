c2ffaf297efb6df3e8f6609d2fd7ca90f38c6d2e
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2021-03-26 01:01:21 +0900

    Change the default of envoy.cert_auto_gen to false

--- modules/aws/scalardl/locals.tf
+++ modules/aws/scalardl/locals.tf
@@ -79,7 +79,7 @@ locals {
     tag                       = "v1.14.1"
     image                     = "envoyproxy/envoy"
     tls                       = false
-    cert_auto_gen             = true
+    cert_auto_gen             = false
     custom_config_path        = ""
   }
 }

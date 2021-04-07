5244605f57a0de44c6ae1681c2764fdf53bda32e
Yusuke Morimoto <yusuke.morimoto@scalar-labs.com>
2021-04-07 11:34:06 +0900

    Remove outputs from cassandra module

--- modules/aws/cassandra/output.tf
+++ modules/aws/cassandra/output.tf
@@ -1,8 +1,3 @@
-output "cassandra_provision_ids" {
-  value       = module.cassandra_provision.provision_ids
-  description = "The IDs of the provisioning step."
-}
-
 output "cassandra_host_ips" {
   value       = module.cassandra_cluster.private_ip
   description = "A list of host IP addresess for the Cassandra cluster."
@@ -36,11 +31,6 @@ output "network_interface_ids" {
 output "cassandra_resource_count" {
   value       = local.cassandra.resource_count
   description = "The number of Cassandra nodes to create."
-}
-
-output "cassandra_start_on_initial_boot" {
-  value       = local.cassandra.start_on_initial_boot
-  description = "A flag to start Cassandra or not on the initial boot."
 }
 
 output "inventory_ini" {

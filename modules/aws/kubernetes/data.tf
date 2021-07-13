6672db424b61043ebbe40fb84872fcc102e558f2
tei-k <tei@leosys.space>
2021-07-13 10:21:00 +0900

    Fix fmt

--- modules/aws/kubernetes/data.tf
+++ modules/aws/kubernetes/data.tf
@@ -5,12 +5,12 @@ data "aws_partition" "current" {}
 data "http" "wait_for_cluster" {
   count = local.kubernetes_cluster.manage_aws_auth ? 1 : 0
 
-  url            = format("%s/healthz", aws_eks_cluster.eks_cluster.endpoint)
-  ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  timeout        = 300
-
   depends_on = [
     aws_eks_cluster.eks_cluster,
     aws_security_group.eks_cluster
   ]
+
+  url            = format("%s/healthz", aws_eks_cluster.eks_cluster.endpoint)
+  ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  timeout        = 300
 }

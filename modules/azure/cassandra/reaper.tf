98bfaa41761df434fd4e3fe9b05aa57ba668f405
inv-jishnu <jishnu.j@innovaturelabs.com>
2026-04-02 15:33:22 +0530

    OS update related changes

--- modules/azure/cassandra/reaper.tf
+++ modules/azure/cassandra/reaper.tf
@@ -1,5 +1,5 @@
 module "reaper_cluster" {
-  source = "github.com/scalar-labs/terraform-azurerm-compute?ref=af49eab-update"
+  source = "github.com/scalar-labs/terraform-azurerm-compute?ref=af49eab-update-os"
 
   nb_instances                  = local.reaper.resource_count
   admin_username                = local.user_name
